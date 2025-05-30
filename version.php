2cebac2a767dedf190bec1a9f00b494da6ee501f
Petr Skoda <petr@mutms.org>
2025-05-30 17:45:18 +0200

    use dev release

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->version   = 2025053000;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.4-10';
+$plugin->release   = 'mu-4.5.4-10+';
