4def98694793edcefd8575836e95d11da9494237
Petr Skoda <petr@mutms.org>
2025-06-09 12:42:41 +0200

    Fix incorrect release

--- version.php
+++ version.php
@@ -32,4 +32,4 @@ $plugin->version   = 2025060900;
 $plugin->requires  = 2024100700;
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->supported = [405, 405];
-$plugin->release   = 'mu-4.5.5-1';
+$plugin->release   = 'mu-4.5.5-01';
