dfba8daf61987c6fd74f1c023f3c557ff30439ec
Petr Skoda <petr@mutms.org>
2026-09-18 11:49:46 +0200

    Moodle 5.3dev migration

--- version.php
+++ version.php
@@ -28,8 +28,8 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muloginas';
-$plugin->version = 2026091350;
-$plugin->requires = 2025041400;
-$plugin->supported = [500, 502];
+$plugin->version = 2026091953;
+$plugin->requires = 2026091600;
+$plugin->supported = [503, 503];
 
-$plugin->release = 'v5.0.10.01';
+$plugin->release = 'v5.3.0.00';
